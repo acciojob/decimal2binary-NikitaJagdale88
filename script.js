3ccae30adf7af98ac200404ddabf6442d5765335
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-06-16 21:41:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // function threeSum(arr, target){
 // }
 // module.exports = threeSum;
-
-function decToBin(num){
-
-	let pow=1;
-	let bin=0;
-
-	while(num!=0){
-		let rem=num%2;
-		bin=bin+rem*pow;
-		pow=pow*10;
-		num=parseInt(num/2);
-	}
-	return bin;
+function threeSum(arr, target) {
+ let binary = "";
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
+
+module.exports = threeSum;
+// function decToBin(num){
+
+// 	let pow=1;
+// 	let bin=0;
+
+// 	while(num!=0){
+// 		let rem=num%2;
+// 		bin=bin+rem*pow;
+// 		pow=pow*10;
+// 		num=parseInt(num/2);
+// 	}
+// 	return bin;
+// }
 
 module.exports=decToBin;
